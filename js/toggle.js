fd98f6c77fd271602fd6ed1055ae986ef7be236b
Coquelicot <yu.xb@outlook.com>
2021-11-24 20:08:53 -0500

    remove useless sections

---
+++ js/toggle.js
@@ -0,0 +1,18 @@
+var status = "less";
+
+function toggleText()
+{
+     var text="Here is some text that I want added to the HTML file";
+    
+	text=document.getElementById("textArea2").innerHTML
+	
+    if (status == "less") {
+        document.getElementById("textArea").innerHTML=text;
+        document.getElementById("toggleButton").innerText = "See Less";
+        status = "more";
+    } else if (status == "more") {
+        document.getElementById("textArea").innerHTML = "";
+        document.getElementById("toggleButton").innerText = "See More";
+        status = "less"
+    }
+}
